4e948fddce3b72d3cf30bb523eaff53938b5dfeb
zubin <404286846@qq.com>
2026-06-22 20:51:49 +0800

    update: 页面补充

--- backend/internal/service/update_service_test.go
+++ backend/internal/service/update_service_test.go
@@ -62,3 +62,39 @@ func TestUpdateServicePerformUpdateNoUpdateReturnsSentinel(t *testing.T) {
 	require.True(t, errors.Is(err, ErrNoUpdateAvailable))
 	require.ErrorIs(t, err, ErrNoUpdateAvailable)
 }
+
+func TestUpdateServiceUnversionedBuildSkipsUpstreamUpdate(t *testing.T) {
+	svc := NewUpdateService(
+		&updateServiceCacheStub{},
+		&updateServiceGitHubClientStub{},
+		"0.0.0-my2g.20260618.1",
+		"release",
+	)
+
+	info, err := svc.CheckUpdate(context.Background(), true)
+
+	require.NoError(t, err)
+	require.Equal(t, "0.0.0-my2g.20260618.1", info.CurrentVersion)
+	require.Equal(t, info.CurrentVersion, info.LatestVersion)
+	require.False(t, info.HasUpdate)
+	require.Nil(t, info.ReleaseInfo)
+}
+
+func TestIsUnversionedBuild(t *testing.T) {
+	tests := []struct {
+		version string
+		want    bool
+	}{
+		{version: "0.0.0-my2g.20260618.1", want: true},
+		{version: "v0.0.0+custom.1", want: true},
+		{version: "0.0.0", want: false},
+		{version: "0.1.138-rc.1", want: false},
+		{version: "0.1.138", want: false},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.version, func(t *testing.T) {
+			require.Equal(t, tt.want, isUnversionedBuild(tt.version))
+		})
+	}
+}
